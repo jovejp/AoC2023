79fe748de71c0bd384e0898cc19258b6cc8355bf
Jove <jove.zhou@nnlife.co.jp>
2023-12-07 17:53:53 +0900

    day7

--- day7/day7.py
+++ day7/day7.py
@@ -1,36 +1,49 @@
-from collections import defaultdict
-import numpy as py
+def find_type(data):
+    check_list = []
+    for x in data:
+        check_list.append(data.count(x))
+    y = max(check_list)
+    if y == 2:
+        yx = int(check_list.count(2) / 2) - 1
+    elif y == 3:
+        yx = int(check_list.count(2) / 2)
+    else:
+        yx = 0
+    return y, yx
 
 
-def read_file_day6(file_name):
-    line_data = defaultdict(list)
+def rep_str(data):
+    src_char = ["A", "K", "Q", "J", "T"]
+    rep_char = ["Z", "Y", "X", "W", "V"]
+    for i, d in enumerate(src_char):
+        data = data.replace(d, rep_char[i])
+    return data
+
+
+def read_file_day7(file_name):
+    line_data = []
     f = open(file_name)
-    line_data["time"] = [int(x) for x in (f.readline().split(":")[1].split())]
-    line_data["distance"] = [int(x) for x in (f.readline().split(":")[1].split())]
+    line = f.readline()
+    while line:
+        row_data = line.split()
+        y, yx = find_type(row_data[0])
+        line_data.append([rep_str(row_data[0]), y, yx, int(row_data[1])])
+        line = f.readline()
     f.close()
+    print(line_data)
+    line_data = sorted(line_data, key=lambda x: (x[1], x[2], x[0]))
     return line_data
 
 
-def day_6(main_list):
-    times = main_list["time"]
-    dists = main_list["distance"]
-    result_list = []
-    for index, time in enumerate(times):
-        count = 0
-        for x in range(time):
-            if x * (time - x) > dists[index]:
-                count += 1
-            else:
-                if count > 0:
-                    break
-        result_list.append(count)
-    return py.prod(result_list)
+def day_7(main_list):
+    return_result = 0
+    for index, data in enumerate(main_list):
+        return_result += (index + 1) * data[3]
+    return return_result
 
 
 if __name__ == '__main__':
-    tmp_list = read_file_day6("day7.txt")
+    tmp_list = read_file_day7("day7.txt")
     print(tmp_list)
-    result_list = day_6(tmp_list)
-    # # # p1
-    print("P1")
+    result_list = day_7(tmp_list)
     print(result_list)
--- day7/day7_2.py
+++ day7/day7_2.py
@@ -1,45 +1,56 @@
-from collections import defaultdict
-import time
+def find_type(data):
+    check_list = []
+    y = 0
+    yx = 0
+    for x in data:
+        if x != "J":
+            check_list.append(data.count(x))
+    if check_list:
+        y = max(check_list)
+    if y == 1:
+        if data.count("J") > 0:
+            yx = 0
+    elif y == 2:
+        yx = int(check_list.count(2) / 2) - 1
+    elif y == 3:
+        yx = int(check_list.count(2) / 2)
+    else:
+        yx = 0
+    return y + data.count("J"), yx
 
 
-def read_file_day6(file_name):
-    line_data = defaultdict(list)
+def rep_str(data):
+    src_char = ["A", "K", "Q", "J", "T"]
+    rep_char = ["Z", "Y", "X", "1", "V"]
+    for i, d in enumerate(src_char):
+        data = data.replace(d, rep_char[i])
+    return data
+
+
+def read_file_day7(file_name):
+    line_data = []
     f = open(file_name)
-    line_data["time"] = int (f.readline().split(":")[1].replace(" ",""))
-    line_data["distance"] = int (f.readline().split(":")[1].replace(" ",""))
+    line = f.readline()
+    while line:
+        row_data = line.split()
+        y, yx = find_type(row_data[0])
+        line_data.append([rep_str(row_data[0]), y, yx, int(row_data[1]), row_data[0]])
+        line = f.readline()
     f.close()
+    # print(line_data)
+    line_data = sorted(line_data, key=lambda x: (x[1], x[2], x[0]))
     return line_data
 
 
-def day_6(main_list):
-    game_time = main_list["time"]
-    dist = main_list["distance"]
-    count = 0
-    start_num = int(dist / game_time)
-    print(start_num)
-    x = start_num
-    while x < game_time:
-        # print("x", x, "count", count)
-        if x * (game_time - x) > dist and (x + start_num) * (game_time - x - start_num) > dist:
-            x = x + start_num
-            count = count + start_num
-        elif x * (game_time - x) > dist:
-            count += 1
-            x += 1
-        else:
-            x += 1
-            if count > 0:
-                break
-    return count
+def day_7(main_list):
+    return_result = 0
+    for index, data in enumerate(main_list):
+        return_result += (index + 1) * data[3]
+    return return_result
 
 
 if __name__ == '__main__':
-    t = time.process_time()
-    tmp_list = read_file_day6("day7.txt")
+    tmp_list = read_file_day7("day7.txt")
     # print(tmp_list)
-    result_list = day_6(tmp_list)
-    # p2
-    # print("P2")
+    result_list = day_7(tmp_list)
     print(result_list)
-    elapsed_time = time.process_time() - t
-    print(elapsed_time)
