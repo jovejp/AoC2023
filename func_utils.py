95fc8cc66e92f6382f44b84d4f868bfa87e81a2a
Jove - Japan <jovechina@163.com>
2023-12-11 00:48:14 +0900

    Day9 (#11)
    * day9
    
    * day10
    
    ---------
    
    Co-authored-by: wower <wower@test-pc.local>

--- func_utils.py
+++ func_utils.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 # read file to array of array, split by blank line
 def read_file_arrays(file_name):
     f = open(file_name)
@@ -12,6 +15,86 @@ def read_file_arrays(file_name):
             my_sub_list.append(line)
         line = f.readline()
     my_main_list.append(my_sub_list)
+    f.close()
+    return my_main_list
+
+
+# read file to array
+def read_file_array(file_name):
+    f = open(file_name)
+    my_main_list = []
+    line = f.readline()
+    while line:
+        my_main_list.append(line.strip())
+        line = f.readline()
+    f.close()
+    return my_main_list
+
+
+# # sample read
+#   1024364543 1121869540 764570177
+# =>
+#   my_main_list = [
+#       [1024364543, 1121869540, 764570177]
+#       [...]
+#   ]
+def read_file_as_int_arrays(file_name):
+    f = open(file_name)
+    my_main_list = []
+    line = f.readline()
+    while line:
+        my_main_list.append([int(x.strip()) for x in line.strip().split()])
+        line = f.readline()
+    f.close()
+    return my_main_list
+
+
+# # sample read
+#   1024364543 1121869540 764570177
+# =>
+#   my_main_list = [
+#       ['1024364543', '1121869540', '764570177']
+#       [...]
+#   ]
+def read_file_as_str_arrays(file_name):
+    f = open(file_name)
+    my_main_list = []
+    line = f.readline()
+    while line:
+        my_main_list.append([x.strip() for x in line.strip().split()])
+        line = f.readline()
+    f.close()
+    return my_main_list
+
+
+# # sample read
+#   Time:        45     98     83     73
+# =>
+#   my_main_list[Time] = [45, 98, 83, 73]
+def read_file_as_int_dict_arrays(file_name, lsp=":", rsp=" "):
+    f = open(file_name)
+    my_main_list = defaultdict(list)
+    line = f.readline()
+    while line:
+        line_data = line.strip().split(lsp)
+        my_main_list[line_data[0]] = [int(x.strip()) for x in line_data[1].strip().split(rsp)]
+        line = f.readline()
+    f.close()
+    return my_main_list
+
+
+# # sample read
+#   Time:        45     98     83     73
+# =>
+#   my_main_list[Time] = ['45', '98', '83', '73']
+def read_file_dict_arrays(file_name, lsp=":", rsp=" "):
+    f = open(file_name)
+    my_main_list = defaultdict(list)
+    line = f.readline()
+    while line:
+        line_data = line.strip().split(lsp)
+        my_main_list[line_data[0]] = [x.strip() for x in line_data[1].strip().split(rsp)]
+        line = f.readline()
     f.close()
     return my_main_list
 
@@ -37,19 +120,13 @@ def read_file_step(file_name, step):
     return my_main_list
 
 
-# read file to array
-def read_file_array(file_name):
-    f = open(file_name)
-    my_main_list = []
-    line = f.readline()
-    while line:
-        my_main_list.append(line.strip())
-        line = f.readline()
-    f.close()
-    return my_main_list
-
-
-# read file to a int array of array, split by 1 character number
+# # sample read
+#   ..730
+# =>
+#   my_main_list= [
+#       ['.', '.', '7', '3', '0']
+#         [...]
+#     ]
 def read_file_array_of_array(file_name):
     f = open(file_name)
     my_main_list = []
