9f12bd5f186b1c3073eb0b8652c6b3df14866792
wower <wower@test-pc.local>
2023-12-18 19:43:26 +0900

    day18

--- day18/func.py
+++ day18/func.py
@@ -1,31 +1,45 @@
-from func_utils import read_file_array_of_array
-import numpy as np
-
-import sys
-
-sys.setrecursionlimit(2000000)
+from func_utils import read_file_array
 
 debug = False
 file_name = "input_sample.txt" if debug else "input.txt"
 
-input_list = read_file_array_of_array(file_name)
-total_rows = len(input_list)
-total_cols = len(input_list[0])
-print("total_rows", total_rows)
-print("total_cols", total_cols)
+input_list = read_file_array(file_name)
+node_list = [(0, 0)]
+action_list = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
 
 
-def func():
-    return 0
+def func(func_part2):
+    total_steps = 0
+    # get all nodes
+    for row_data in input_list:
+        action, step, color = row_data.split()
+        if not func_part2:
+            step = int(step)
+            step_row, step_col = action_list[action]
+        else:
+            step = int(color[2:-2], 16)
+            action = "RDLU"[int(color[-2:-1])]
+            step_row, step_col = action_list[action]
+        cur_row, cur_col = node_list[-1]
+        node_list.append((cur_row + step_row * step, cur_col + step_col * step))
+        total_steps += step
+
+    # area
+    total_rows = len(node_list)
+    total_area = abs(sum(node_list[i][0] * (node_list[i - 1][1] - node_list[(i + 1) % total_rows][1])
+                         for i in range(total_rows))) // 2
+    return total_area + total_steps // 2 + 1
 
 
 def part1():
-    result = func()
+    result = func(False)
     return result
 
 
 def part2():
-    result = func()
+    node_list.clear()
+    node_list.append((0, 0))
+    result = func(True)
     return result
 
 
