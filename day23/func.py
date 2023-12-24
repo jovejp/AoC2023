a514aafc4109268882e4dafe4bb882809267bcb5
wower <wower@test-pc.local>
2023-12-25 00:16:01 +0900

    bug fix

--- day23/func.py
+++ day23/func.py
@@ -1,7 +1,7 @@
 from func_utils import read_file_array_of_array
 from collections import deque
 
-debug = True
+debug = False
 file_name = "input_sample.txt" if debug else "input.txt"
 
 input_list = read_file_array_of_array(file_name)
@@ -10,38 +10,37 @@ total_cols = len(input_list[0])
 print("total_rows", total_rows)
 print("total_cols", total_cols)
 
-dirs = [[0, -1], [-1, 0], [0, 1], [1, 0]]
-slopes = ["<^>v"]
+dirs = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+slopes = ["^", "<", "v", ">"]
 his_path = {}
 
 
-def dfs(sp):
+def dfs(sp, p2=False):
     max_path = 1
     qw = deque()
-    qw.append((1, sp, []))
+    qw.append((1, sp, [sp]))
     while qw:
         cur_path, cur_sp, cur_moved_nodes = qw.pop()
-        # print("new round started:", cur_path, cur_sp, cur_moved_nodes)
         if cur_sp[0] == total_rows - 1:
             his_path[cur_path] = cur_moved_nodes
-            continue
-        if input_list[cur_sp[0]][cur_sp[1]] in slopes:
-            nd = dirs[slopes.index(input_list[sp[0]][sp[1]])]
-            ns = [a + b for a, b in zip(cur_sp, nd)]
-            if input_list[ns[0]][ns[1]] == ".":
-                qw.append((cur_path, ns, cur_moved_nodes + [ns]))
-            else:
-                print("this way is blocked!!!", cur_path, ns, cur_moved_nodes)
-        else:
-            ns = [[nd[0] + cur_sp[0], nd[1] + cur_sp[1]] for nd in dirs
-                  if input_list[nd[0] + cur_sp[0]][nd[1] + cur_sp[1]] != "#"
-                  and [nd[0] + cur_sp[0], nd[1] + cur_sp[1]] not in cur_moved_nodes]
-
-            # print("cur_path, cur_sp, ns", cur_path, cur_sp, ns)
-
-            for idx, yy in enumerate(ns):
+            break  # continue <- just lucy to find in first search
+        idx = 0
+        for nd in dirs:
+            yy = [nd[0] + cur_sp[0], nd[1] + cur_sp[1]]
+            if yy in cur_moved_nodes:
+                continue
+            if (input_list[yy[0]][yy[1]] in slopes
+                    and (p2 or dirs[slopes.index(input_list[yy[0]][yy[1]])] == nd)):
                 if idx == 0:
                     qw.append((cur_path, yy, cur_moved_nodes + [yy]))
+                    idx = 1
+                else:
+                    max_path += 1
+                    qw.append((max_path, yy, cur_moved_nodes + [yy]))
+            elif input_list[yy[0]][yy[1]] == ".":
+                if idx == 0:
+                    qw.append((cur_path, yy, cur_moved_nodes + [yy]))
+                    idx = 1
                 else:
                     max_path += 1
                     qw.append((max_path, yy, cur_moved_nodes + [yy]))
@@ -49,19 +48,26 @@ def dfs(sp):
 
 
 def part1():
-    all_path = dfs([0, 1])
-    result = max([len(x) for x in all_path.values()])
-    return result
+    all_path = dfs([0, 1], False)
+    result = 0
+    for x in all_path.values():
+        # print(len(x))
+        result = max(result, len(x))
+    return result - 1
 
 
 def part2():
-    all_path = dfs([0, 1])
-    result = max([len(x) for x in all_path.values()])
-    return result
+    his_path.clear()
+    all_path = dfs([0, 1], True)
+    if all_path:
+        result = max([len(x) for x in all_path.values()])
+    else:
+        result = 0
+    return result - 1
 
 
 if __name__ == '__main__':
-    # r_p1 = part1()
-    # print("part1", r_p1)
-    r_p2 = part2()
-    print("part2", r_p2)
+    r_p1 = part1()
+    print("part1", r_p1)
+    # r_p2 = part2()
+    # print("part2", r_p2)
